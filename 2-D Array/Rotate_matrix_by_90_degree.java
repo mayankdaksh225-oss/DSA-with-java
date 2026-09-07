fa40716bf26851e2605b31a541e6f6a9776e5513
mayankdaksh225-oss <mayankdaksh225@gmail.com>
2026-09-07 16:20:11 +0530

    Add files via upload

---
+++ 2-D Array/Rotate_matrix_by_90_degree.java
@@ -0,0 +1,24 @@
+public class Rotate_matrix_by_90_degree {
+//    public static int reverse()
+    public static void main(String[] args) {
+//        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+//        for(int i = 0; i<arr.length; i++){
+//            for(int j = 0; j<=i; j++){
+//                int temp = arr[i][j];
+//                arr[i][j] = arr[j][i];
+//                arr[j][i] = temp;
+//
+////                reverse();
+//            }
+//
+//        }
+//        for(int i =0; i<arr.length; i++){
+//            for(int j = arr[0].length-1; j>=0; j--){
+//                System.out.print(arr[i][j]+" ");
+//            }
+//            System.out.println();
+        //}
+
+
+    }
+}
